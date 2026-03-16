75d40fcdc73cea031cff4face456b6f2e1ac6bc1
YOOOOOOOOoor <tyordi111@gmail.com>
2026-03-17 00:49:51 +0300

    connect backend to Railway database

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // REQUIRED for Railway
+  },
 });
 
 export default pool;
