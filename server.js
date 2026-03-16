d109fc44f9aacabfe4fb4d770044d41285f5a005
YOOOOOOOOoor <tyordi111@gmail.com>
2026-03-17 00:42:59 +0300

    fix railway dynamic port

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use(
     credentials: true,
   }),
 );
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productsRoutes);
 app.use("/api/seller", sellerRoutes);
