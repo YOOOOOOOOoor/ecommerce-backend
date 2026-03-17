3a4c90057c10d41ac35464ae4b9e0d23e027e3af
Yor <164674888+YOOOOOOOOoor@users.noreply.github.com>
2026-03-17 17:56:49 +0300

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.js";
 import productsRoutes from "./routes/products.js";
 import sellerRoutes from "./routes/seller.js";
@@ -10,26 +11,27 @@ import cartsRoutes from "./routes/carts.js";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cookieParser());
+// CORS for frontend
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
+    credentials: true, // allow cookies
+  })
 );
-app.get("/", (req, res) => {
-  res.send("API is running 🚀");
-});
 
+app.use(cookieParser());
+app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productsRoutes);
 app.use("/api/seller", sellerRoutes);
 app.use("/api/carts", cartsRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
 });
+
+app.listen(PORT, () => console.log("Server running on port", PORT));
