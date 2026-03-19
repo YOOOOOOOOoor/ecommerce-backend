855cb9feeedf875e9227d3f999160c0010181123
YOOOOOOOOoor <tyordi111@gmail.com>
2026-03-19 14:45:45 +0300

    update backend changes

--- config/db.js
+++ config/db.js
@@ -1,14 +1,29 @@
 import pkg from "pg";
 const { Pool } = pkg;
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Railway DATABASE_URL
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-});
+let pool;
+
+if (process.env.DB_HOST) {
+  // Local DB is uncommented
+  pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
+} else {
+  // Production DB
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
+  });
+}
 
 export default pool;
