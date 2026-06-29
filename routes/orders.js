337a7250cbb62a780e7bfec3632b09c66747edbd
Yor <164674888+YOOOOOOOOoor@users.noreply.github.com>
2026-06-29 14:52:27 +0300

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,1 +1,24 @@
 
+import express from "express";
+import protect from "../middleware/auth.js";
+import pool from "../config/db.js";
+
+const router = express.Router();
+
+router.post("/", protect, async (req, res) => {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    // We'll build this step by step
+  } catch (err) {
+    await client.query("ROLLBACK");
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  } finally {
+    client.release();
+  }
+});
+
+export default router;
