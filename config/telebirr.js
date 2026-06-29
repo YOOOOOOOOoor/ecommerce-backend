8e49c95ecd704d1ccb29dd9e1316b810756a818d
Yor <164674888+YOOOOOOOOoor@users.noreply.github.com>
2026-06-29 15:30:54 +0300

    Update telebirr.js

--- config/telebirr.js
+++ config/telebirr.js
@@ -1,1 +1,23 @@
 
+export const telebirrConfig = {
+  baseUrl:
+    "https://developerportal.ethiotelebirr.et:38443/apiaccess/payment/gateway",
+
+  webBaseUrl:
+    "https://developerportal.ethiotelebirr.et:38443/payment/web/paygate?",
+
+  fabricAppId:
+    process.env.TELEBIRR_FABRIC_APP_ID,
+
+  appSecret:
+    process.env.TELEBIRR_APP_SECRET,
+
+  merchantAppId:
+    process.env.TELEBIRR_MERCHANT_APP_ID,
+
+  merchantCode:
+    process.env.TELEBIRR_SHORT_CODE,
+
+  privateKey:
+    process.env.TELEBIRR_PRIVATE_KEY,
+};
